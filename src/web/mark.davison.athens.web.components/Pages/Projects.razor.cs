6848ee702494f87b792e42dd6e60739c3bd16cc8
mark.davison <markdavison0+github@gmail.com>
2023-12-24 14:39:43 +1300

    basic projects page, with colour border

---
+++ src/web/mark.davison.athens.web.components/Pages/Projects.razor.cs
@@ -0,0 +1,22 @@
+﻿using mark.davison.athens.shared.models.dtos.Shared;
+
+namespace mark.davison.athens.web.components.Pages;
+
+public partial class Projects
+{
+    public List<ProjectDto> LoadedProjects { get; } = new();
+
+    protected override Task OnInitializedAsync()
+    {
+        LoadedProjects.AddRange(
+            [
+                new ProjectDto { Id = Guid.NewGuid(), Name = "Project #1" },
+                new ProjectDto { Id = Guid.NewGuid(), Name = "Project #2" },
+                new ProjectDto { Id = Guid.NewGuid(), Name = "Project #3" },
+                new ProjectDto { Id = Guid.NewGuid(), Name = "Project #4" },
+                new ProjectDto { Id = Guid.NewGuid(), Name = "Project #5" },
+                new ProjectDto { Id = Guid.NewGuid(), Name = "Project #6 but some really long text that will overflow the container that it is in" },
+            ]);
+        return base.OnInitializedAsync();
+    }
+}
